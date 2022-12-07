2f0d64cdcd13491d0974cdbb1fe88b2e8e9a7c92
MorganHolsters <64076952+MorganHolsters@users.noreply.github.com>
2022-12-07 22:05:15 +0100

    Login - Added logo + CSS

--- screens/CustomInput/CustomInput.js
+++ screens/CustomInput/CustomInput.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {View, Text, TextInput, StyleSheet } from 'react-native';
+import { shouldUseActivityState } from "react-native-screens";
 
 const CustomInput = () => {
     return (
@@ -11,8 +12,20 @@ const CustomInput = () => {
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    input: {},
+    container: {
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        borderstyle: 'solid',
+
+    },
+    input: {
+        paddingTop: 10,
+        borderstyle: 'solid',
+        paddingBottom: 10,
+        textAlign: "center",
+        width: '50%',
+        backgroundColor: '#D3D3D3',
+    },
 });
 
 export default CustomInput
--- screens/SignInScreen/SignInScreen.js
+++ screens/SignInScreen/SignInScreen.js
@@ -1,10 +1,15 @@
-import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
-import CustomInput from "../CustomInput";
+import React from 'react';
+import { View, Text, StyleSheet, useWindowDimensions } from 'react-native';
+import CustomInput from '../CustomInput';
+//import Logo from 'C:\github\projet-integration-4-Parking\assets\ParkNRide.png';
+//<Image source={Logo} style={[styles.logo, {height: height * 0.3}]} />
+
 
 const SignInScreen = () => {
+    const {height} = useWindowDimensions();
     return (
         <View style={styles.root}>
+            
             <Text>Login</Text>
             <CustomInput />
         </View>
@@ -15,7 +20,13 @@ const styles = StyleSheet.create({
     root:{
         alignItems: 'center',
         padding: 20,
+        textAlign: 'center',
     },
+    logo: {
+        width: '70%',
+        maxWidth: 100,
+        maxHeight: 100,
+    }
 });
 
 export default SignInScreen;
