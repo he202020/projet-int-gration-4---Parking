71dbc2175ac9bae164a45bca84f2b13c4caf1326
LibonNoe <noe@libon.eu>
2023-08-02 18:32:05 +0200

    Restructuration de la table SQL "parking"
    1. create_table_parking.sql => création de la table "parking" avec comme attributs:
        - l'id
        - le nom
        - l'heure d'ouverture
        - l'heure de fermeture
        - l'adresse
        - le nombre de places maximum
        - la longitute
        - la latitude
    
    2. insert_into_parking.sql => insertion de données par défaut dans la table "parking"

--- backend/sql/tables/create_table_parking.sql
+++ backend/sql/tables/create_table_parking.sql
@@ -1,15 +1,10 @@
-CREATE TABLE IF NOT EXISTS public.parking
-(
-    parking_id character varying(10) COLLATE pg_catalog."default" NOT NULL,
-    parking_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    parking_opening_hour time without time zone NOT NULL,
-    parking_closure_hour time without time zone NOT NULL,
-    parking_address character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    parking_maximum_place integer,
-    CONSTRAINT parking_pkey PRIMARY KEY (parking_id)
-    )
-
-    TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.parking
-    OWNER to g4;
+CREATE TABLE parking (
+    id INT NOT NULL,
+    name VARCHAR NOT NULL,
+    opening TIME NULL,
+    closure TIME NULL,
+    address VARCHAR NOT NULL,
+    max INT NOT NULL,
+    longitude FLOAT NOT NULL,
+    latitude FLOAT NOT NULL
+);
---
+++ backend/sql/tables/insert_into_parking.sql
@@ -0,0 +1,5 @@
+INSERT INTO parking (id, name, opening, closure, address, max, longitude, latitude)
+VALUES
+    (1, 'Magritte', '08:30:00', '18:00:00', 'Av. du Ciseau 10, 1348 Ottignies-Louvain-la-Neuve', 50, 4.611498, 50.665886),
+    (2, 'Leclercq', '09:00:00', '19:00:00', 'Bd du S, 1348 Ottignies-Louvain-la-Neuve', 60, 4.612858, 50.666845),
+    (3, 'Wallons', '07:00:00', '16:30:00', '1348 Ottignies-Louvain-la-Neuve', 55, 4.617058, 50.669534);
