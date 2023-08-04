14bab82724e9ee52fd36ec4ff0a47aec5d0d7b14
LibonNoe <noe@libon.eu>
2023-08-04 17:03:40 +0200

    Restructuration de la table SQL "person"
    1. create_table_person.sql => création de la table "person" avec comme attributs:
        - l'id
        - le prénom
        - le nom de famille
        - l'adresse email
        - le numéro de téléphone
        - la plaque d'immatriculation
        - le droit admin
    
    2. insert_into_parking.sql => insertion de données par défaut dans la table "person"
        - un superuser par défaut
        - des users par défaut

---
+++ backend/sql/tables/insert_into_person.sql
@@ -0,0 +1,8 @@
+INSERT INTO person (first_name, last_name, email, numberplate, is_admin)
+VALUES
+    ('John', 'Doe', 'john@doe.be', '1-AAA-111', TRUE);
+
+INSERT INTO person (first_name, last_name, email, numberplate)
+VALUES
+    ('Noé', 'Libon', 'noe@libon.be', '1-AAA-112'),
+    ('Pauline', 'Vanderhulst', 'pauline@vanderhulst.be', '1-AAA-113');
